511f9484d2e315eb3fb81c0f9ba68531b274ef19
zamzamnn <zamzamnoorahmed73@gmail.com>
2024-05-19 20:52:15 +0300

    done-functions-continued lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*') {
+  return function(zamzam = 'special') {
+    return `You are ${flair}${zamzam}${flair}!`;
+  };
+}
